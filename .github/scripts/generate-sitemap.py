4f0b0f0b84e467732b661d039ec621090efda6a8
Leah Dykema <103963317+leahdykema@users.noreply.github.com>
2025-05-24 09:57:09 -0500

    Update to exclude pages in development

--- .github/scripts/generate-sitemap.py
+++ .github/scripts/generate-sitemap.py
@@ -10,7 +10,11 @@ EXCLUDE_PATHS = {
     "html/thankyou.html",
     "html/dino.html",
     "html/apple-store-escape.html",
-    "html/trivia.html"
+    "html/trivia.html",
+    # in developmet
+    "html/support.html",
+    "html/service.html",
+    "html/service-form.html"
 }
 OUTPUT_FILE = "sitemap.xml"
 
